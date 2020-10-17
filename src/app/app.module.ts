249cb482306880d59b637261906a289e84525b02
clennam <amkahandawala@gmail.com>
2020-10-17 16:56:39 +0530

    Add timer and list components to layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
