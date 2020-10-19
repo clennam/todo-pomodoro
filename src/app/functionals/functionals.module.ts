dcb0ce0fc7eaff474d696134263580a89f477950
clennam <amkahandawala@gmail.com>
2020-10-20 03:04:09 +0530

    create timerDisplayPipe to display MM:SS format

--- src/app/functionals/functionals.module.ts
+++ src/app/functionals/functionals.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimerWidgetComponent } from './timer-widget/timer-widget.component';
+import { TimerDisplayPipe } from './pipes/timer-display/timer-display.pipe';
 
 @NgModule({
   declarations: [
-    TimerWidgetComponent
+    TimerWidgetComponent,
+    TimerDisplayPipe
   ],
   imports: [
     CommonModule
---
+++ src/app/functionals/pipes/timer-display/timer-display.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/**
+ * Converts a given value in seconds to the format MM:SS for display.
+ */
+@Pipe({
+  name: 'timerDisplay'
+})
+export class TimerDisplayPipe implements PipeTransform {
+
+  /**
+   * Converts a given value in seconds to the format MM:SS
+   * @param fullTime time in seconds
+   */
+  transform(fullTime: number): string {
+    let minutes: string = Math.floor(fullTime/60).toString().padStart(2, '0');
+    let seconds: string = (fullTime%60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  }
+
+}
